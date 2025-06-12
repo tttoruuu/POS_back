60bc2c18f9a348e9b12365c83eea8293c2fd661b
toru <toru09may@gmail.com>
2025-06-12 18:10:53 +0900

    start

--- startup.sh
+++ startup.sh
@@ -2,10 +2,13 @@
 
 # 仮想環境の作成と有効化
 python -m venv venv
-source venv/bin/activate
+source antenv/bin/activate
 
 # 依存関係のインストール
 pip install -r requirements.txt
+
+# $PORTがセットされていなければ8000を使う
+PORT=${PORT:-8000}
 
 # Gunicornでアプリケーションを起動
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT 
